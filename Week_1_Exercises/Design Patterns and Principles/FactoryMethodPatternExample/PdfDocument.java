9f5a19fb1f53b511d26608b8459bbbd109b6cf8d
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-07-31 19:21:25 +0530

    Create PdfDocument.java

---
+++ Week_1_Exercises/Design Patterns and Principles/FactoryMethodPatternExample/PdfDocument.java
@@ -0,0 +1,7 @@
+package FactoryMethodPatternExample;
+
+public class PdfDocument implements Document{
+    public void Writing(){
+        System.out.println("I am Writing in Pdf Documents");
+    }
+}
