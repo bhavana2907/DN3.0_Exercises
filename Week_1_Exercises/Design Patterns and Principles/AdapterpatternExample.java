7b97647cc4fb33d2b680b8a9342b567c0eb4e0a7
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-07-31 19:29:04 +0530

    Create AdapterpatternExample.java

---
+++ Week_1_Exercises/Design Patterns and Principles/AdapterpatternExample.java
@@ -0,0 +1,75 @@
+package AdapterPatternExample;
+
+interface PaymentHandler {
+    void handlePayment(double amount);
+}
+
+class PayPalService {
+    public void executePayment(double amount) {
+        System.out.println("Processing payment of Rs." + amount + " through PayPal.");
+    }
+}
+
+class StripeService {
+    public void execute(double amount) {
+        System.out.println("Processing payment of Rs." + amount + " through Stripe.");
+    }
+}
+
+class AmazonPayService {
+    public void process(double amount) {
+        System.out.println("Processing payment of Rs." + amount + " through Amazon Pay.");
+    }
+}
+
+class PayPalHandlerAdapter implements PaymentHandler {
+    private PayPalService payPalService;
+
+    public PayPalHandlerAdapter(PayPalService payPalService) {
+        this.payPalService = payPalService;
+    }
+
+    public void handlePayment(double amount) {
+        payPalService.executePayment(amount);
+    }
+}
+
+class StripeHandlerAdapter implements PaymentHandler {
+    private StripeService stripeService;
+
+    public StripeHandlerAdapter(StripeService stripeService) {
+        this.stripeService = stripeService;
+    }
+
+    public void handlePayment(double amount) {
+        stripeService.execute(amount);
+    }
+}
+
+class AmazonPayHandlerAdapter implements PaymentHandler {
+    private AmazonPayService amazonPayService;
+
+    public AmazonPayHandlerAdapter(AmazonPayService amazonPayService) {
+        this.amazonPayService = amazonPayService;
+    }
+
+    public void handlePayment(double amount) {
+        amazonPayService.process(amount);
+    }
+}
+
+public class AdapterPatternExample {
+    public static void main(String[] args) {
+        PayPalService payPalService = new PayPalService();
+        StripeService stripeService = new StripeService();
+        AmazonPayService amazonPayService = new AmazonPayService();
+
+        PaymentHandler payPalAdapter = new PayPalHandlerAdapter(payPalService);
+        PaymentHandler stripeAdapter = new StripeHandlerAdapter(stripeService);
+        PaymentHandler amazonPayAdapter = new AmazonPayHandlerAdapter(amazonPayService);
+
+        payPalAdapter.handlePayment(100.00);
+        stripeAdapter.handlePayment(200.00);
+        amazonPayAdapter.handlePayment(300.00);
+    }
+}
