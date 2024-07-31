2eaf07ba3b12f24f8c2a74d62fdab62da79d58cf
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-07-31 19:22:12 +0530

    Create WordDocument.java

---
+++ Week_1_Exercises/Design Patterns and Principles/FactoryMethodPatternExample/WordDocument.java
@@ -0,0 +1,7 @@
+package FactoryMethodPatternExample;
+
+public class WordDocument implements Document{
+    public void Writing(){
+        System.out.println("I am Writing in Word Documents");
+    }
+}
