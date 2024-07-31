f84df0670f7c52a08f6e90b8c44d92043af9963e
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-07-31 19:20:30 +0530

    Create FactoryTest.java

---
+++ Week_1_Exercises/Design Patterns and Principles/FactoryMethodPatternExample/FactoryTest.java
@@ -0,0 +1,10 @@
+package FactoryMethodPatternExample;
+
+public class FactoryTest {
+    public static void main(String args[]){
+        FactoryWordDocument w=new FactoryWordDocument();
+        w.createDocument();
+        
+    }
+    
+}
