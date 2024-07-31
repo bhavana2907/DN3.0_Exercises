1428b457ba6f5e9b9dc34f96a7b9cc3b285a8668
Bhavana <132038004+bhavana2907@users.noreply.github.com>
2024-07-31 19:17:03 +0530

    Create ExcelDocument.java

---
+++ Week_1_Exercises/Design Patterns and Principles/FactoryMethodPatternExample/ExcelDocument.java
@@ -0,0 +1,7 @@
+package FactoryMethodPatternExample;
+
+public class ExcelDocument implements Document{
+    public void Writing(){
+        System.out.println("I am Writing in Excel Documents");
+    }
+}
